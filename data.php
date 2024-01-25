193fd24ea68c31c357a9adda8cd874fdcbd87f36
ArnevaATS <136663265+ArnevaATS@users.noreply.github.com>
2024-01-25 13:22:59 +0700

    aaaa

--- data.php
+++ data.php
@@ -5,20 +5,52 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>My Map </title>
+    <title>Absen Geo Tagging</title>
 </head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <body style="background-color: #ccc">
 
 <div class="container">
+    <h3 class="text-center">Data Absensi</h3>
+    <hr>
     <div class="row">
-        <div class="col-md-8 offset-2 text-center p-5" style="border:1px solid #000; background-color: #fff">
-        <?php
-            $sql = mysqli_query($kon,"select * from absen"); 
-            while($data = mysqli_fetch_array($sql)){
-                echo '<iframe src="https://maps.google.com/maps?q='.$data['latitude'].','.$data['longitude'].'&hl=en&z=14&amp;output=embed" width="100%" height="450" allowfullscreen=""></iframe>';
-            }
-        ?>
+        <div class="col-lg-12" style="border:1px solid #000; background-color: #fff">
+        <div class="table-responsive">
+        <table class="table">
+            <caption>List of Pengguna</caption>
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Photo</th>
+                <th scope="col">Latitude</th>
+                <th scope="col">langitude</th>
+                <th scope="col">User Login</th>
+                <th scope="col">Tanggal Masuk</th>
+                <th scope="col">Token</th>
+                <th scope="col">Maps </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = mysqli_query($kon,"select * from absen"); 
+                    while($data = mysqli_fetch_array($sql)){
+                    ?>
+                     <tr>
+                        <th scope="row"><?php echo $data['id']; ?></th>
+                        <td><img src="<?php echo $data['foto'];?>" width="300"/></td>
+                        <td><?php echo $data['latitude'];?></td>
+                        <td><?php echo $data['longitude'];?></td>
+                        <td><?php echo $data['userlogin'];?></td>
+                        <td><?php echo $data['tanggal'];?></td>
+                        <td><?php echo $data['token'];?></td>
+                        <td><iframe src="https://maps.google.com/maps?q=<?php echo $data['latitude'];?>,<?php echo $data['longitude'];?>&hl=en&z=14&amp;output=embed" width="300" height="250" allowfullscreen=""></iframe></td>
+                    </tr>
+                    <?php
+                    }
+                ?>
+            </tbody>
+            </table>
+            </div>
         </div>
     </div>
 <div>
--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         </div>
     </div>
 <div>
-    
+
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -70,7 +70,7 @@ function takeSnapshot() {
             console.log(hr?.responseText??"");
                 if(hr?.responseText??"" == 'Oke'){
                     document.getElementById('video-webcam').style='display:none';
-                    document.getElementById("pesan").appendChild("Berhasil Ambil Gambar");
+                    document.getElementById("pesan").innerHTML="Berhasil Ambil Gambar";
                 }else{
                     alert("Gagal Ambil Photo");
                     return false;
@@ -79,11 +79,7 @@ function takeSnapshot() {
     }
 hr.send(imgData); 
 }
-</script>
 
-
-
-<script>
 function absenMasuk(){
     
     if (navigator.geolocation) {
@@ -111,4 +107,8 @@ function showPosition(position) {
     ajax.send(vars);
 }
 
+
+function absenKeluar(){
+    window.location='data.php';
+}
 </script>
